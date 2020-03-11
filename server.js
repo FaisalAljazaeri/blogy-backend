5d5391e680162148dcf5cb305742f4d333df1d99
FaisalAljazaeri <faisal.hassan93@gmail.com>
2020-03-11 15:48:05 +0300

    Add Cors to server

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
+const cors = require("cors");
 const indexRouter = require("./app/routes/index");
 const articlesRouter = require("./app/routes/article");
 
@@ -13,12 +14,21 @@ mongoose.connection.once("open", () => {
     console.log("Connected");
 });
 
+const port = process.env.PORT || 5000;
+const reactPort = 3000;
+
+// Set CORS headers on response from this API using the `cors` NPM package
+app.use(
+    cors({
+        origin: process.env.CLIENT_ORIGIN || `http://localhost:${reactPort}`
+    })
+);
+
 app.use(express.json());
 
 app.use("/", indexRouter);
 app.use("/api/articles", articlesRouter);
 
-const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Server Listening on port ${port}`);
 });
